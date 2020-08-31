09d2ca193f6bbcf70325bb174d0cf1a2d5dd3896
Ian Sadovy <ian.sadovy@gmail.com>
2020-08-31 19:51:13 +0300

    with highcharts demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HighchartsComponent } from './highcharts/highcharts.component';
 import { PivoTableDemoComponent } from './examples/pivo-table-demo/pivo-table-demo.component';
 import { CallingEventsComponent } from './examples/calling-events/calling-events.component';
 import { UsingApiCallsComponent } from './examples/using-api-calls/using-api-calls.component';
 import { UpdatingDataComponent } from './examples/updating-data/updating-data.component';
 import { CustomizingToolbarComponent } from './examples/customizing-toolbar/customizing-toolbar.component';
 import { CustomizingGridComponent } from './examples/customizing-grid/customizing-grid.component';
+import { WithHighchartsComponent } from './examples/with-highcharts/with-highcharts.component';
 
 const appRoutes: Routes = [
     { path: 'pivot-table-demo', component: PivoTableDemoComponent },
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
     { path: 'updating-data', component: UpdatingDataComponent },
     { path: 'customizing-toolbar', component: CustomizingToolbarComponent },
     { path: 'customizing-grid', component: CustomizingGridComponent },
-    { path: 'highcharts', component: HighchartsComponent },
+    { path: 'with-highcharts', component: WithHighchartsComponent },
     { path: '**', redirectTo: 'pivot-table-demo' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 import { FlexmonsterPivotModule } from 'ng-flexmonster';
-import { TabsComponent } from './tabs/tabs.component';
-import { HomeComponent } from './home/home.component';
-import { HighchartsComponent } from './highcharts/highcharts.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TopMenuComponent } from './common/top-menu/top-menu.component';
 import { SideMenuComponent } from './common/side-menu/side-menu.component';
@@ -16,13 +13,11 @@ import { UsingApiCallsComponent } from './examples/using-api-calls/using-api-cal
 import { UpdatingDataComponent } from './examples/updating-data/updating-data.component';
 import { CustomizingToolbarComponent } from './examples/customizing-toolbar/customizing-toolbar.component';
 import { CustomizingGridComponent } from './examples/customizing-grid/customizing-grid.component';
+import { WithHighchartsComponent } from './examples/with-highcharts/with-highcharts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent,
-    HomeComponent,
-    HighchartsComponent,
     TopMenuComponent,
     SideMenuComponent,
     PivoTableDemoComponent,
@@ -30,7 +25,8 @@ import { CustomizingGridComponent } from './examples/customizing-grid/customizin
     UsingApiCallsComponent,
     UpdatingDataComponent,
     CustomizingToolbarComponent,
-    CustomizingGridComponent
+    CustomizingGridComponent,
+    WithHighchartsComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, MatTabsModule, FlexmonsterPivotModule, AppRoutingModule
